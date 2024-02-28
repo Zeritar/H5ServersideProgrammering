a3b2754a6b2a894e04864835c4eba86ed5994d29
Zeritar <mm_9290@hotmail.com>
2024-02-28 14:15:59 +0100

    Done questionmark?

---
+++ Migrations/20240228130004_init.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace H5ServersideProgrammering.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoItems",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserID = table.Column<string>(type: "nvarchar(500)", nullable: false),
+                    ItemText = table.Column<string>(type: "nvarchar(500)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItems", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserCPR",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserID = table.Column<string>(type: "nvarchar(500)", nullable: false),
+                    CPR = table.Column<string>(type: "nvarchar(500)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserCPR", x => x.ID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserCPR_UserID",
+                table: "UserCPR",
+                column: "UserID",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoItems");
+
+            migrationBuilder.DropTable(
+                name: "UserCPR");
+        }
+    }
+}
