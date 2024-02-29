8a47d20e8c8b205a22d395c82353c426d0ae5bf0
Zeritar <mm_9290@hotmail.com>
2024-02-29 08:59:12 +0100

    fix migrate to include db files in build

--- migrate.ps1
+++ migrate.ps1
@@ -1,2 +1,24 @@
 update-database -Context ApplicationDbContext
 update-database -Context AppDataContext
+
+$dir = "."
+$ns = @{ defaultNamespace = "http://schemas.microsoft.com/developer/msbuild/2003" }
+
+$config = [xml] "<ItemGroup>
+    <None Update=`"Data\appData.db`">
+      <CopyToOutputDirectory>Always</CopyToOutputDirectory>
+    </None>
+    <None Update=`"Data\appIdentity.db`">
+      <CopyToOutputDirectory>Always</CopyToOutputDirectory>
+    </None>
+  </ItemGroup>"
+
+Get-ChildItem $dir *.csproj | 
+% { 
+  $content = [xml](gc $_.FullName); 
+  $importNode = $content.ImportNode($config.DocumentElement, $true) 
+  $project = $content.Project;
+  $project
+  $project.AppendChild($importNode);
+  $content.Save($_.FullName);
+}
